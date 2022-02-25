697711206df92aaf245bb6cf8443bbb502c37168
catrefuse <anlocetic@foxmail.com>
2022-02-26 02:44:32 +0800

    ver 1.0

--- src/code.ts
+++ src/code.ts
@@ -1,35 +1,27 @@
 import { dispatch, handleEvent } from './codeMessageHandler';
 figma.showUI(__html__);
 
-// The following shows how messages from the UI code can be handled in the main code.
-handleEvent('createNode', () => {
-    const node = figma.createRectangle();
-    node.name = node.id;
-
-    // This shows how the main code can send messages to the UI code.
-    dispatch('nodeCreated', node.id);
-});
-
-figma.showUI(__html__);
-
 figma.ui.resize(300, 500);
-
-interface Message {
-    type: string;
-    nodeID: string;
-    characters: string;
-}
 
 handleEvent('change-text', async (data) => {
     console.log('begin to change');
 
     const node = figma.getNodeById(data.id) as TextNode;
 
-    const textNameArray = node.getRangeAllFontNames(0, node.characters.length);
+    if (node.characters.length > 0) {
+        const textNameArray = node.getRangeAllFontNames(
+            0,
+            node.characters.length
+        );
 
-    for (const fontName of textNameArray) {
-        await figma.loadFontAsync(fontName);
+        for (const fontName of textNameArray) {
+            await figma.loadFontAsync(fontName);
+        }
+    } else if (node.characters.length == 0) {
+        await figma.loadFontAsync(node.fontName as FontName);
     }
+
+    console.log('font loaded');
 
     node.characters = data.characters;
 });
@@ -80,3 +72,74 @@ function selectionHandler() {
 }
 
 figma.on('selectionchange', selectionHandler);
+
+figma.on('run', selectionHandler);
+
+let rect = null as RectangleNode;
+
+handleEvent('input-focus', (data) => {
+    rect = figma.createRectangle();
+    const textNode = figma.getNodeById(data.id) as TextNode;
+
+    let highlight = clone(rect.fills);
+
+    highlight[0].color = { r: 122 / 256, g: 98 / 256, b: 249 / 256 };
+
+    rect.fills = highlight;
+    const absoluteTextNodePostion = {
+        x: textNode.absoluteTransform[0][2],
+        y: textNode.absoluteTransform[1][2],
+    };
+
+    let width = 0;
+
+    // console.log(textNode.getStyledTextSegments(['fontSize']));
+
+    if (textNode.fontSize !== figma.mixed) {
+        width = 0.3 * (textNode.fontSize as number);
+    } else {
+        width = 0.3 * textNode.getStyledTextSegments(['fontSize'])[0].fontSize;
+    }
+
+    console.log(width);
+
+    rect.x = absoluteTextNodePostion.x - width * 2;
+    rect.y = absoluteTextNodePostion.y;
+    rect.resize(width, textNode.height);
+});
+
+handleEvent('input-blur', (data) => {
+    const textNode = figma.getNodeById(data.id) as TextNode;
+
+    rect.remove();
+    rect = null;
+});
+
+// tools
+
+function clone(val) {
+    const type = typeof val;
+    if (val === null) {
+        return null;
+    } else if (
+        type === 'undefined' ||
+        type === 'number' ||
+        type === 'string' ||
+        type === 'boolean'
+    ) {
+        return val;
+    } else if (type === 'object') {
+        if (val instanceof Array) {
+            return val.map((x) => clone(x));
+        } else if (val instanceof Uint8Array) {
+            return new Uint8Array(val);
+        } else {
+            let o = {};
+            for (const key in val) {
+                o[key] = clone(val[key]);
+            }
+            return o;
+        }
+    }
+    throw 'unknown';
+}
--- src/ui/ui.vue
+++ src/ui/ui.vue
@@ -1,46 +1,135 @@
-<script setup lang='ts'>
-import styles from 'figma-plugin-ds/dist/figma-plugin-ds.css'
-import { onMounted, ref } from 'vue';
-import { handleEvent, dispatch } from './uiMessageHandler';
+<script setup lang="ts">
+import { onMounted, ref, watch } from "vue";
+import { handleEvent, dispatch } from "./uiMessageHandler";
+import "figma-plugin-ds/dist/figma-plugin-ds.css";
+// import { attach } from "@frsource/autoresize-textarea";
 
-let textNodeArray = ref([] as TextNode[])
+let textNodeArray = ref([] as TextNode[]);
 
 onMounted(() => {
-  handleEvent("selectionchange", data => {
+  handleEvent("selectionchange", (data) => {
     console.log("ui detect change");
-    textNodeArray.value = data.textNodeArray
-  })
-})
+    textNodeArray.value = data.textNodeArray;
+    // setTimeout(() => {
+    //   document.getElementById("0")?.focus()
+    // }, 10)
+    console.log(document.getElementsByTagName("textarea"));
+
+    // for (const textarea of document.getElementsByTagName("textarea")) {
+    //   const buffer = textarea.value;
+    //   textarea.value = " ";
+    //   textarea.value = buffer;
+    //   console.log("attach");
+    //   attach(textarea);
+    // }
+  });
+});
 
 function handleChange(event: Event, textNode: TextNode) {
-  dispatch('change-text', { id: textNode.id, characters: textNode.characters })
+  console.log("onchange");
+
+  dispatch("change-text", { id: textNode.id, characters: textNode.characters });
 }
 
+function toNextInput(event: KeyboardEvent, index: number) {
+  console.log("enter");
 
+  if (event.isComposing) {
+    return;
+  }
+  // document.getElementById(`${index + 1}`)?.focus()
+}
+
+function handleFocus(event: FocusEvent, item: TextNode) {
+  dispatch("input-focus", { id: item.id });
+}
+
+function handleBlur(event: FocusEvent, item: TextNode) {
+  dispatch("input-blur", { id: item.id });
+}
 </script>
-
 
 <template>
   <div id="ui">
-    <h3>Text Dash</h3>
-    <p>🖋</p>
+    <div
+      class="type type--large type-bold"
+      v-if="textNodeArray.length == 0"
+    >🧐 select something to edit text inside</div>
+    <!-- <div
+      v-else
+      class="type type--large type-bold"
+    >{{ textNodeArray.length }} text nodes found in your selection</div>-->
+
     <div id="input-container">
-      <div v-for="(item, index) in textNodeArray" :key="index">
-        <h6>name: {{ item.name }}, id: {{ item.id }}</h6>
-        <input type="text" v-model="item.characters" @change="handleChange($event, item)" />
+      <div v-for="(item, index) in textNodeArray" :key="index" class>
+        <div class="head-container">
+          <div class="icon">𝚃</div>
+          <div class="type type--medium type--bold" id="input-head">{{ item.name }}</div>
+        </div>
+
+        <textarea
+          type="text"
+          v-model="item.characters"
+          @input="handleChange($event, item)"
+          @keypress.enter="toNextInput($event, index)"
+          :id="index.toString()"
+          rows="2"
+          class="textarea"
+          @focus="handleFocus($event, item)"
+          @blur="handleBlur($event, item)"
+        />
+      </div>
+    </div>
+
+    <div class="tips">
+      <div class="type">
+        <strong>⇥ tab</strong>: switch to the next one
+      </div>
+      <div class="type">
+        <strong>⇧ shift + ⇥ tab</strong>: switch to the previous one
       </div>
     </div>
   </div>
 </template>
 
-
-
 <style scoped>
 #ui {
+  padding: 16px;
+}
+
+#input-head {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  flex: 1;
+}
+
+.head-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.icon {
+  height: 24px;
+  width: 24px;
+  font-size: 16px;
+  transform: scale(1.2, 1);
+}
+
+#input-container {
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: var(--size-medium);
+  gap: 16px;
+}
+
+.textarea {
+  resize: vertical;
+  min-height: 24px !important;
+}
+
+.tips {
+  margin-top: 16px;
 }
 </style>
